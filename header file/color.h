06832c2f7cf95fc53b47ef7f7f28abaf5be90885
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-26 00:02:52 +0900

    Create color.h

---
+++ header file/color.h
@@ -0,0 +1,24 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using Color = Vec3;
+
+void writeColor(std::ostream& out, const Color& pixelColor) {
+    auto r = pixelColor.getX();
+    auto g = pixelColor.getY();
+    auto b = pixelColor.getZ();
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    // Write out the pixel color components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
