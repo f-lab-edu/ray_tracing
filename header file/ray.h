6088de6c7fec60cc9a4eac490e85a3d7b0afb21f
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-26 00:03:07 +0900

    Create ray.h

---
+++ header file/ray.h
@@ -0,0 +1,24 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class Ray {
+public:
+    Ray() {}
+
+    Ray(const Point3& originInput, const Vec3& directionInput) : origin(originInput), direction(directionInput) {}
+
+    const Point3& getOrigin() const { return origin; }
+    const Vec3& getDirection() const { return direction; }
+
+    Point3 getPosition(double t) const {
+        return origin + t * direction;
+    }
+
+private:
+    Point3 origin;
+    Vec3 direction;
+};
+
+#endif
