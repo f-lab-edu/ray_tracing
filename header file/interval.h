ad090761fa857488ee8166b4981e057c1927ee77
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-27 01:28:27 +0900

    Create interval.h

---
+++ header file/interval.h
@@ -0,0 +1,32 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include "ray_utility.h"
+
+class Interval {
+public:
+    double min, max;
+
+    Interval() : min(+RT_INFINITY), max(-RT_INFINITY) {} // Default interval is empty
+
+    Interval(double min, double max) : min(min), max(max) {}
+
+    double getSize() const {
+        return max - min;
+    }
+
+    bool doesContain(double x) const {
+        return min <= x && x <= max;
+    }
+
+    bool doesSurround(double x) const {
+        return min < x && x < max;
+    }
+
+    static const Interval empty, universe;
+};
+
+const Interval Interval::empty = Interval(+RT_INFINITY, -RT_INFINITY);
+const Interval Interval::universe = Interval(-RT_INFINITY, +RT_INFINITY);
+
+#endif
