3ec174224dc431ab2634de8f1c4e642bc608bbe1
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-27 01:01:48 +0900

    Create hittable_list.h

---
+++ header file/hittable_list.h
@@ -0,0 +1,41 @@
+#ifndef Hittable_LIST_H
+#define Hittable_LIST_H
+
+#include "Hittable.h"
+
+#include <memory>
+#include <vector>
+
+class HittableList : public Hittable {
+public:
+    std::vector<std::shared_ptr<Hittable>> objects;
+
+    HittableList() {}
+    HittableList(std::shared_ptr<Hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+
+    void add(std::shared_ptr<Hittable> object) {
+        objects.push_back(object);
+    }
+
+    bool isHit(const Ray& r, double rayMin, double rayMax, HitRecord& record) const override {
+        HitRecord tempRecord;
+        bool isHitAnything = false;
+        auto closestSoFar = rayMax;
+
+        for (const auto& object : objects) {
+            if (object->isHit(r, rayMin, closestSoFar, tempRecord)) {
+                isHitAnything = true;
+                if (tempRecord.root < closestSoFar) {
+                    closestSoFar = tempRecord.root;
+                    record = tempRecord;
+                }
+            }
+        }
+
+        return isHitAnything;
+    }
+};
+
+#endif
