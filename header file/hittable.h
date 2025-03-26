88bf9b34280530aa1d83dc9e664f2b2ca3b563c5
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-27 00:50:22 +0900

    Create hittable.h

---
+++ header file/hittable.h
@@ -0,0 +1,26 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+struct HitRecord {
+    Point3 hitPosition;
+    Vec3 normalizedVector;
+    double root;
+    bool frontFace;
+
+    void setFaceNormal(const Ray& inputRay, const Vec3& outwardNormal) {
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+        frontFace = performDot(inputRay.getDirection(), outwardNormal) < 0.0;
+        normalizedVector = frontFace ? outwardNormal : -outwardNormal;
+    }
+};
+
+class Hittable {
+public:
+    virtual ~Hittable() = default;
+
+    virtual bool hit(const Ray& inputRay, double rayMin, double rayMax, HitRecord& record) const = 0;
+};
+
+#endif
