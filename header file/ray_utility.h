016533ed82cb3912d213054ebc0a3abebe393551
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-27 01:03:16 +0900

    Create ray_utility.h

---
+++ header file/ray_utility.h
@@ -0,0 +1,24 @@
+#ifndef RAYUTILITY_H
+#define RAYUTILITY_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+
+// Constants
+constexpr double RT_INFINITY = std::numeric_limits<double>::infinity();
+constexpr double PI = 3.1415926535897932385;
+
+// Utility Functions
+inline double convertDegreesToRadians(double degrees) {
+    return degrees * PI / 180.0;
+}
+
+// Common Headers
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
