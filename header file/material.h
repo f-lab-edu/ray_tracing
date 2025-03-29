137698fde5502557f86cfae50895b0e03e9b0b00
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-29 14:22:48 +0900

    Update material.h
    apply fuzz reflection to metal

--- header file/material.h
+++ header file/material.h
@@ -35,17 +35,24 @@ private:
 
 class Metal : public Material {
 public:
-    Metal(const Color& inputAlbedo) : albedo(inputAlbedo) {}
+    Metal(const Color& inputAlbedo, double inputFuzz) : albedo(inputAlbedo), fuzz(inputFuzz) {
+        if (inputFuzz < -1)
+            inputFuzz = -1;
+        else if (inputFuzz > 1)
+            inputFuzz = 1;
+    }
 
     bool doesScatter(const Ray &inputRay, const HitRecord &record, Color &attenuation, Ray &scatteredRay) const override {
         Vec3 reflectedVector = getReflectedMirror(inputRay.getDirection(), record.normalizedVector);
+        reflectedVector = getUnitVector(reflectedVector) + (fuzz * getRandomUnitVector());
         scatteredRay = Ray(record.hitPosition, reflectedVector);
         attenuation = albedo;
-        return true;
+        return (performDot(scatteredRay.getDirection(), record.normalizedVector) > 0);
     }
 
 private:
     Color albedo;
+    double fuzz;
 };
 
 #endif
