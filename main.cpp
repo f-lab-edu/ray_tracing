86ca8c5aa7c464a810e479dcfeaf1da093e4243d
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-29 14:02:14 +0900

    Update and rename source code/main.cpp to main.cpp
    apply metal

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
+#include "material.h"
 #include "sphere.h"
 
 double isHitSphere(const Point3& sphereCenter, double sphereRadius, const Ray& inputRay) {
@@ -21,8 +22,16 @@ double isHitSphere(const Point3& sphereCenter, double sphereRadius, const Ray& i
 int main() {
     HittableList world;
 
-    world.add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5));
-    world.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100));
+    auto materialGround = std::make_shared<Lambertian>(Color(0.8, 0.8, 0.0));
+    auto materialCenter = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
+    auto materialLeft = std::make_shared<Metal>(Color(0.8, 0.8, 0.8));
+    auto materialRight = std::make_shared<Metal>(Color(0.8, 0.6, 0.2));
+
+
+    world.add(std::make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, materialGround));
+    world.add(std::make_shared<Sphere>(Point3(0.0, 0.0, -1.2), 0.5, materialCenter));
+    world.add(std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
+    world.add(std::make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, materialRight));
 
     Camera camera;
 
