d46335ad13a249e503d28ff30d8bc7a5198d1a0f
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-29 13:02:49 +0900

    Create material.h

---
+++ header file/material.h
@@ -0,0 +1,30 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "hittable.h"
+
+class Material {
+public:
+    virtual ~Material() = default;
+
+    virtual bool doesScatter(const Ray &inputRay, const HitRecord &record, Color &attenuation, Ray &scatteredRay) const {
+        return false;
+    }
+};
+
+class Lambertian : public Material {
+public:
+    Lambertian(const Color& inputAlbedo) : albedo(inputAlbedo) {}
+
+    bool doesScatter(const Ray &inputRay, const HitRecord &record, Color &attenuation, Ray &scatteredRay) const override {
+        auto scatterDirection = record.normalizedVector + getRandomUnitVector();
+        scatteredRay = Ray(record.hitPosition, scatterDirection);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    Color albedo;
+};
+
+#endif
