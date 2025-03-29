fa2a84d98f711c3a4347119c78d6ce3cb7a9baaf
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-29 13:42:53 +0900

    Update material.h
    implement Metal class

--- header file/material.h
+++ header file/material.h
@@ -32,4 +32,20 @@ private:
     Color albedo;
 };
 
+
+class Metal : public Material {
+public:
+    Metal(const Color& inputAlbedo) : albedo(inputAlbedo) {}
+
+    bool doesScatter(const Ray &inputRay, const HitRecord &record, Color &attenuation, Ray &scatteredRay) const override {
+        Vec3 reflectedVector = getReflectedMirror(inputRay.getDirection(), record.normalizedVector);
+        scatteredRay = Ray(record.hitPosition, reflectedVector);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    Color albedo;
+};
+
 #endif
