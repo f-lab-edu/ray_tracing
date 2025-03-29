d50db43c4e69227f3bcac24f71008794ea1b93af
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-03-30 00:46:33 +0900

    Update material.h
    apply Schlick Approximation

--- header file/material.h
+++ header file/material.h
@@ -70,7 +70,7 @@ public:
 
         bool canRefracted = finalRefractionIndex * sinTheta > 1.0;
         Vec3 finalRay;      // this can be made from reflection or refraction
-        if (canRefracted)
+        if (canRefracted || getReflectance(cosTheta, finalRefractionIndex) > getRandomDouble(0, 1))
             finalRay = getReflectedMirror(normalizedInputVector, record.normalizedVector);
         else
             finalRay = getRefracted(normalizedInputVector, record.normalizedVector, finalRefractionIndex);
@@ -83,6 +83,13 @@ private:
     // Refractive index in vacuum or air, or the ratio of the material's refractive index over
     // the refractive index of the enclosing media
     double refractionIndex;
+
+    static double getReflectance(double cosine, double inputRefractionIndex) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - inputRefractionIndex) / (1 + inputRefractionIndex);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
 };
 
 #endif
