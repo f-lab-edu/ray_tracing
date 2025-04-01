1ed70e0f9bfd9417a4f0bd126e44fcc9716f913c
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-04-01 23:17:14 +0900

    Update texture.h
    implement spatial texture

--- header file/texture.h
+++ header file/texture.h
@@ -11,6 +11,7 @@ public:
     virtual Color getColor(double u, double v, const Point3& position) const = 0;
 };
 
+
 class ConstantTexture : public Texture {
 public:
     ConstantTexture(const Color& albedo) : albedo(albedo) {}
@@ -23,6 +24,32 @@ public:
 
 private:
     Color albedo;
+};
+
+class CheckerTexture  : public Texture {
+public:
+    CheckerTexture (double scale, std::shared_ptr<Texture> even, std::shared_ptr<Texture> odd)
+        : inverseScale(1.0 / scale), textureForEven(even), textureForOdd(odd) {
+    }
+
+    CheckerTexture (double scale, const Color& c1, const Color& c2)
+        : CheckerTexture (scale, std::make_shared<ConstantTexture>(c1), std::make_shared<ConstantTexture>(c2)) {
+    }
+
+    Color getColor(double u, double v, const Point3& position) const override {
+        auto xInteger = int(std::floor(inverseScale * position.getX()));
+        auto yInteger = int(std::floor(inverseScale * position.getY()));
+        auto zInteger = int(std::floor(inverseScale * position.getZ()));
+
+        bool isEven = (xInteger + yInteger + zInteger) % 2 == 0;
+
+        return isEven ? textureForEven->getColor(u, v, position) : textureForOdd->getColor(u, v, position);
+    }
+
+private:
+    double inverseScale;
+    std::shared_ptr<Texture> textureForEven;
+    std::shared_ptr<Texture> textureForOdd;
 };
 
 #endif
