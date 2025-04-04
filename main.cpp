3d9a9d02f0b338123b8395eae49029c1fccf0a13
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-04-05 02:02:51 +0900

    Update main.cpp
    change names

--- main.cpp
+++ main.cpp
@@ -162,18 +162,18 @@ void renderQuads() {
     HittableList world;
 
     // Materials
-    auto left_red = std::make_shared<Lambertian>(Color(1.0, 0.2, 0.2));
-    auto back_green = std::make_shared<Lambertian>(Color(0.2, 1.0, 0.2));
-    auto right_blue = std::make_shared<Lambertian>(Color(0.2, 0.2, 1.0));
-    auto upper_orange = std::make_shared<Lambertian>(Color(1.0, 0.5, 0.0));
-    auto lower_teal = std::make_shared<Lambertian>(Color(0.2, 0.8, 0.8));
+    auto leftRed = std::make_shared<Lambertian>(Color(1.0, 0.2, 0.2));
+    auto backGreen = std::make_shared<Lambertian>(Color(0.2, 1.0, 0.2));
+    auto rightBlue = std::make_shared<Lambertian>(Color(0.2, 0.2, 1.0));
+    auto upperOrange = std::make_shared<Lambertian>(Color(1.0, 0.5, 0.0));
+    auto lowerTeal = std::make_shared<Lambertian>(Color(0.2, 0.8, 0.8));
 
     // Quads
-    world.add(std::make_shared<Quad>(Point3(-3, -2, 5), Vec3(0, 0, -4), Vec3(0, 4, 0), left_red));
-    world.add(std::make_shared<Quad>(Point3(-2, -2, 0), Vec3(4, 0, 0), Vec3(0, 4, 0), back_green));
-    world.add(std::make_shared<Quad>(Point3(3, -2, 1), Vec3(0, 0, 4), Vec3(0, 4, 0), right_blue));
-    world.add(std::make_shared<Quad>(Point3(-2, 3, 1), Vec3(4, 0, 0), Vec3(0, 0, 4), upper_orange));
-    world.add(std::make_shared<Quad>(Point3(-2, -3, 5), Vec3(4, 0, 0), Vec3(0, 0, -4), lower_teal));
+    world.add(std::make_shared<Quad>(Point3(-3, -2, 5), Vec3(0, 0, -4), Vec3(0, 4, 0), leftRed));
+    world.add(std::make_shared<Quad>(Point3(-2, -2, 0), Vec3(4, 0, 0), Vec3(0, 4, 0), backGreen));
+    world.add(std::make_shared<Quad>(Point3(3, -2, 1), Vec3(0, 0, 4), Vec3(0, 4, 0), rightBlue));
+    world.add(std::make_shared<Quad>(Point3(-2, 3, 1), Vec3(4, 0, 0), Vec3(0, 0, 4), upperOrange));
+    world.add(std::make_shared<Quad>(Point3(-2, -3, 5), Vec3(4, 0, 0), Vec3(0, 0, -4), lowerTeal));
 
     Camera camera;
 
