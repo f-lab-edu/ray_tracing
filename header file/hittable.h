bd13fcedcc37b203cdf24ed905af496fe0984ca9
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-04-06 22:03:37 +0900

    Update hittable.h
    implement translation

--- header file/hittable.h
+++ header file/hittable.h
@@ -29,4 +29,34 @@ public:
     virtual AABB getBoundingBox() const = 0;
 };
 
+
+class Translate : public Hittable {
+public:
+    Translate(std::shared_ptr<Hittable> inputObject, const Vec3& inputOffset) : baseObject(inputObject), offset(inputOffset) {
+        boundingBox = baseObject->getBoundingBox() + offset;
+    }
+
+    bool isHit(const Ray& inputRay, Interval timeIntervalToCheck, HitRecord& record) const override {
+        // Move the ray backwards by the offset
+        Ray adjustedRay(inputRay.getOrigin() - offset, inputRay.getDirection(), inputRay.getTime());
+
+        // Determine whether an intersection exists along the offset ray (and if so, where)
+        if (baseObject->isHit(adjustedRay, timeIntervalToCheck, record) == false)
+            return false;
+        
+        // Move the intersection point forwards by the offset
+        record.hitPosition += offset;
+        return true;
+    }
+    AABB getBoundingBox() const override {
+        return boundingBox;
+    }
+
+private:
+    std::shared_ptr<Hittable> baseObject;
+    Vec3 offset;
+    AABB boundingBox;
+};
+
+
 #endif
