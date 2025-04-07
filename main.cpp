265da457c022fe5ff491c4166c6100a969fd310a
sadoe3 <68961439+sadoe3@users.noreply.github.com>
2025-04-07 21:30:56 +0900

    Update main.cpp
    complete week 2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 #include "bvh.h"
 #include "camera.h"
-#include "hittable.h"
+#include "constant_medium.h"
+#include "Hittable.h"
 #include "hittable_list.h"
 #include "material.h"
 #include "Quad.h"
@@ -43,7 +44,7 @@ void renderBouncingSpheres() {
                     world.add(std::make_shared<Sphere>(center, center2, 0.2, sphereMaterial));
                 }
                 else if (randomMaterial < 0.95) {
-                    // metal
+                    // Metal
                     auto albedo = Color::getRandomVector(0.5, 1);
                     auto fuzz = getRandomDouble(0, 0.5);
                     sphereMaterial = std::make_shared<Metal>(albedo, fuzz);
@@ -283,6 +284,131 @@ void renderCornellBox() {
 }
 
 
+void renderCornellSmoke() {
+    HittableList world;
+
+    auto red = std::make_shared<Lambertian>(Color(.65, .05, .05));
+    auto white = std::make_shared<Lambertian>(Color(.73, .73, .73));
+    auto green = std::make_shared<Lambertian>(Color(.12, .45, .15));
+    auto light = std::make_shared<DiffuseLight>(Color(7, 7, 7));
+
+    world.add(std::make_shared<Quad>(Point3(555, 0, 0), Vec3(0, 555, 0), Vec3(0, 0, 555), green));
+    world.add(std::make_shared<Quad>(Point3(0, 0, 0), Vec3(0, 555, 0), Vec3(0, 0, 555), red));
+    world.add(std::make_shared<Quad>(Point3(113, 554, 127), Vec3(330, 0, 0), Vec3(0, 0, 305), light));
+    world.add(std::make_shared<Quad>(Point3(0, 555, 0), Vec3(555, 0, 0), Vec3(0, 0, 555), white));
+    world.add(std::make_shared<Quad>(Point3(0, 0, 0), Vec3(555, 0, 0), Vec3(0, 0, 555), white));
+    world.add(std::make_shared<Quad>(Point3(0, 0, 555), Vec3(555, 0, 0), Vec3(0, 555, 0), white));
+
+    std::shared_ptr<Hittable> box1 = getBox(Point3(0, 0, 0), Point3(165, 330, 165), white);
+    box1 = std::make_shared<RotateY>(box1, 15);
+    box1 = std::make_shared<Translate>(box1, Vec3(265, 0, 295));
+
+    std::shared_ptr<Hittable> box2 = getBox(Point3(0, 0, 0), Point3(165, 165, 165), white);
+    box2 = std::make_shared<RotateY>(box2, -18);
+    box2 = std::make_shared<Translate>(box2, Vec3(130, 0, 65));
+
+    world.add(std::make_shared<ConstantMedium>(box1, 0.01, Color(0, 0, 0)));
+    world.add(std::make_shared<ConstantMedium>(box2, 0.01, Color(1, 1, 1)));
+
+    Camera camera;
+
+    camera.aspectRatio = 1.0;
+    camera.imageWidth = 600;
+    camera.samplesPerPixel = 200;
+    camera.maxDepth = 50;
+    camera.backgroundColor = Color(0, 0, 0);
+
+    camera.verticalFOV = 40;
+    camera.lookFrom = Point3(278, 278, -800);
+    camera.lookAt = Point3(278, 278, 0);
+    camera.upVector = Vec3(0, 1, 0);
+
+    camera.defocusAngle = 0;
+
+    camera.render(world);
+}
+
+
+void renderFinalScene(int imageWidth, int samplesPerPixel, int maxDepth) {
+    HittableList boxes1;
+    auto ground = std::make_shared<Lambertian>(Color(0.48, 0.83, 0.53));
+
+    int boxesPerSide = 20;
+    for (int i = 0; i < boxesPerSide; i++) {
+        for (int j = 0; j < boxesPerSide; j++) {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i * w;
+            auto z0 = -1000.0 + j * w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = getRandomDouble(1, 101);
+            auto z1 = z0 + w;
+
+            boxes1.add(getBox(Point3(x0, y0, z0), Point3(x1, y1, z1), ground));
+        }
+    }
+
+    HittableList world;
+
+    world.add(std::make_shared<BVHNode>(boxes1));
+
+    auto light = std::make_shared<DiffuseLight>(Color(7, 7, 7));
+    world.add(std::make_shared<Quad>(Point3(123, 554, 147), Vec3(300, 0, 0), Vec3(0, 0, 265), light));
+
+    auto center1 = Point3(400, 400, 200);
+    auto center2 = center1 + Vec3(30, 0, 0);
+    auto sphereMaterial = std::make_shared<Lambertian>(Color(0.7, 0.3, 0.1));
+    world.add(std::make_shared<Sphere>(center1, center2, 50, sphereMaterial));
+
+    world.add(std::make_shared<Sphere>(Point3(260, 150, 45), 50, std::make_shared<Dielectric>(1.5)));
+    world.add(std::make_shared<Sphere>(
+        Point3(0, 150, 145), 50, std::make_shared<Metal>(Color(0.8, 0.8, 0.9), 1.0)
+    ));
+
+    auto boundary = std::make_shared<Sphere>(Point3(360, 150, 145), 70, std::make_shared<Dielectric>(1.5));
+    world.add(boundary);
+    world.add(std::make_shared<ConstantMedium>(boundary, 0.2, Color(0.2, 0.4, 0.9)));
+    boundary = std::make_shared<Sphere>(Point3(0, 0, 0), 5000, std::make_shared<Dielectric>(1.5));
+    world.add(std::make_shared<ConstantMedium>(boundary, .0001, Color(1, 1, 1)));
+
+    auto emat = std::make_shared<Lambertian>(std::make_shared<ImageTexture>("earthmap.jpg"));
+    world.add(std::make_shared<Sphere>(Point3(400, 200, 400), 100, emat));
+    auto pertext = std::make_shared<NoiseTexture>(0.2);
+    world.add(std::make_shared<Sphere>(Point3(220, 280, 300), 80, std::make_shared<Lambertian>(pertext)));
+
+    HittableList boxes2;
+    auto white = std::make_shared<Lambertian>(Color(.73, .73, .73));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(std::make_shared<Sphere>(Point3::getRandomVector(0, 165), 10, white));
+    }
+
+    world.add(std::make_shared<Translate>(
+        std::make_shared<RotateY>(
+            std::make_shared<BVHNode>(boxes2), 15),
+        Vec3(-100, 270, 395)
+    )
+    );
+
+    Camera camera;
+
+    camera.aspectRatio = 1.0;
+    camera.imageWidth = imageWidth;
+    camera.samplesPerPixel = samplesPerPixel;
+    camera.maxDepth = maxDepth;
+    camera.backgroundColor = Color(0, 0, 0);
+
+    camera.verticalFOV = 40;
+    camera.lookFrom = Point3(478, 278, -600);
+    camera.lookAt = Point3(278, 278, 0);
+    camera.upVector = Vec3(0, 1, 0);
+
+    camera.defocusAngle = 0;
+
+    camera.render(world);
+}
+
+
 
 int main() {
     //renderBouncingSpheres();
@@ -291,7 +417,9 @@ int main() {
     //renderPerlinSpheres();
     //renderQuads();
     //renderSimpleLight();
-    renderCornellBox();
-    
+    //renderCornellBox();
+    //renderCornellSmoke();
+    renderFinalScene(800, 10000, 40);   // or 400, 250, 4
+
     return 0;
 }
